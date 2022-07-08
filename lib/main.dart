7643d1242535b44fa4b59b356630a59f5f8c29fe
Tayyab Mughal <tayyabmughal676@gmail.com>
2022-07-08 15:05:33 +0500

    Push Notification Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qbus/L10n/L10n.dart';
@@ -10,17 +11,58 @@ import 'package:qbus/local_cache/utils.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async{
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  debugPrint('Handling a background message ${message.messageId}');
+}
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //Initializing the firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await FirebaseMessaging.instance
+      .setForegroundNotificationPresentationOptions(
+        alert: true, // Required to display a heads up notification
+        badge: true,
+        sound: true,
+      )
+      .then((value) =>
+          {debugPrint("setForegroundNotificationPresentationOption")});
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    debugPrint('Got a message whilst in the foreground!');
+    debugPrint('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      debugPrint(
+          'Message also contained a notification: ${message.notification!.title}');
+      if (message.data['type'] == 'chat') {
+        debugPrint("Type is Chat Opened");
+      }
+    }
+  });
+
+  FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    debugPrint("onMessageOpenedApp: ${event.data}");
+    if (event.data['type'] == 'chat') {
+      debugPrint("Type is Chat Opened");
+    }
+  });
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
