b0c0df3a81ffcac29ddff2b4edfa5bccadfe61ec
Mr. Tayyab Mughal <tayyabmughal676@gmail.com>
2023-02-03 12:34:24 +0500

    Added Localization

--- lib/screens/bottombar/bottom_bar_screens/setting_select_lang_screens/setting_select_lang_screen.dart
+++ lib/screens/bottombar/bottom_bar_screens/setting_select_lang_screens/setting_select_lang_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qbus/utils/constant.dart';
 import 'package:qbus/widgets/custom_text.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SettingScreenSelectLang extends StatefulWidget {
   const SettingScreenSelectLang({Key? key}) : super(key: key);
